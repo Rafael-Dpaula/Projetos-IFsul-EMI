62b7c6a62234225744654c319aea39b2e6b69534
Rafael-Dpaula <160616087+Rafael-Dpaula@users.noreply.github.com>
2024-04-19 20:39:04 -0300

    Delete Programação directory

--- Programação/atividades 14.02/att2/att2.js
+++
@@ -1,8 +0,0 @@
-function att2(){
-var num1 = parseFloat(prompt("digite um número"));
-var num2 = parseFloat(prompt("digite outro número"));
-
-var result = num1 * num2;
-console.log("o resultado é: " + result);
-return
-}
